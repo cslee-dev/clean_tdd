d6570a516b5c4956e28ef9e0412f520dcbd0c5dd
lcs <cslee@itmoulds.com>
2021-01-20 19:33:01 +0900

    첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
-def lists_main(request):
-    return HttpResponse("hi", status=200)
+def home_page(request):
+    pass
